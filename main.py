53a9f7a4d0ace17706ddb8bdb1560d33e2a05bd1
Keith Wolters <kowolters@gmail.com>
2026-01-20 14:15:54 -0500

    added timer

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -18,6 +20,8 @@ def main():
             
         screen.fill('black')
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
