3cde3968d3023bd4ffcda9df3d3a8e2118a1b888
Keith Wolters <kowolters@gmail.com>
2026-01-31 07:52:51 -0500

    added movement

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ def main():
                 return
             
         screen.fill('black')
+        dt = 0
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
